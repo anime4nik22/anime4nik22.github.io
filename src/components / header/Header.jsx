373619acc1e722d715ed872ca6952891f1577bce
anime4nik22 <kirillcernakov2555@gmail.com>
2023-11-13 09:44:44 +0700

    Add main frame

--- src/components / header/Header.jsx
+++ src/components / header/Header.jsx
@@ -1,13 +1,37 @@
-import styles from './Header.module.css'
+import './header.css'
 
 function Header () {
     return (
-        <header className={styles.header}>
+        <header className="header">
             <div className="container">
                 <div className='header__row'>
                     <div className='header__logo'>Logo</div>
-                    <div className='header__nav'>Nav</div>
+                    <nav className='header__nav'>
+                        <ul>
+                            <li>
+                                <a href="#!">Главная</a>
+                            </li>
+                            <li>
+                                <a href="#!">Контакты</a>
+                            </li>
+                            <li>
+                                <a href="#!">Доставка</a>
+                            </li>
+                            <li>
+                                <a href="#!">ДСП</a>
+                            </li>
+                            <li>
+                                <a className="header__nav-btn" href="#!">ВОЙТИ</a>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
+            </div>
+            <div className='container'>
+                <div className='text__numb'>01</div>
+                <div className='text__glav'>Доставка <br></br> лесоматериалов <br></br> любой сложности</div>
+                <div className="text__desc">Перевозим материалы <br></br> по всей России. Возьмёмся за <br></br> работу любой сложности. <br></br> Высококвалифицированный рабочий <br></br> персонал. Имеются <br></br> страховки </div>
+                <div className="text__cont">+7 (603) 555-0123</div>            
             </div>
         </header>
     )
---
+++ src/components /promo/Promo.jsx
@@ -0,0 +1,14 @@
+import './promo.css'
+
+const Promo = () => {
+    return (<section className="promo">
+            <div className="container">
+                <div className="promo__content">
+                    <div className="promo__text">Текст</div>
+                    <div className="promo__img">Картинка</div>
+            </div>
+        </div>
+    </section>);
+}
+
+export default Promo;
